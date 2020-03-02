d029b911d7a76dcd1c9dbe7610d8139885808796
pikun <57031274+pikunthong@users.noreply.github.com>
2020-03-02 13:57:54 +0700

    update3

--- app/src/main/java/th/ac/su/monsterfarm/MainActivity.kt
+++ app/src/main/java/th/ac/su/monsterfarm/MainActivity.kt
@@ -2,11 +2,17 @@ package th.ac.su.monsterfarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import th.ac.su.monsterfarm.Utils.getJsonDataFromAsset
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val jsonFileString = getJsonDataFromAsset(applicationContext,"monster_data.json")
+
+        Log.i("data",jsonFileString)
     }
 }
